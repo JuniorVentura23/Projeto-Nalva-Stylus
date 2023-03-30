fff14bb9625f055707620834d65e3ebdbecce762
Junior Ventura <jr.cori@hotmail.com>
2023-03-30 11:25:18 -0300

    NavBar fechando resp

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,13 @@
+/*=========== Menu Navbar =============*/
+
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a) {
+    a.addEventListener("click", () => {
+        navCollapse.classList.remove("show");
+    });
+});
+
 // //Sessão counter
 
 let valueDisplays = document.querySelectorAll(".num");
